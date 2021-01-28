91a63ffa104c69b07f1b967f2b6c912cbc0f5d5b
Sandro0330 <sandro030486@gmail.com>
2021-01-28 00:07:21 -0300

    Carregamento lento (Lazy Loading) na busca dos dados

---
+++ projeto-java-jpa/src/main/java/br/com/jpa/teste/TestaRelatorioDasMovimentacoes.java
@@ -0,0 +1,30 @@
+package br.com.jpa.teste;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import br.com.jpa.modelo.Conta;
+
+public class TestaRelatorioDasMovimentacoes {
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
+		EntityManager em = emf.createEntityManager();
+
+		String jpql = "select c from Conta c";
+		TypedQuery<Conta> query = em.createQuery(jpql, Conta.class);
+		
+		List<Conta> contas = query.getResultList();
+		for(Conta conta : contas) {
+			System.out.println("Titular: " + conta.getTitular());
+			System.out.println("Agencia: " + conta.getAgencia());
+			System.out.println("Número: " + conta.getNumero());
+			System.out.println("Movimentações: " + conta.getMovimentacoes());
+		}
+		
+	}
+	
+}
