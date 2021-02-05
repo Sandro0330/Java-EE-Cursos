b332a20322f5cdf048fe60659c5b86f84d999a5b
Sandro0330 <sandro030486@gmail.com>
2021-02-05 13:49:11 -0300

    Criando um objetos simples para enviar menssagem o Producer

--- agendamento-email/src/main/java/com/jakarta/job/AgendamentoEmailJob.java
+++ agendamento-email/src/main/java/com/jakarta/job/AgendamentoEmailJob.java
@@ -2,9 +2,13 @@ package com.jakarta.job;
 
 import java.util.List;
 
+import javax.annotation.Resource;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.inject.Inject;
+import javax.jms.JMSConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.Queue;
 
 import com.jakarta.entidade.AgendamentoEmail;
 import com.jakarta.servico.AgendamentoEmailServico;
@@ -15,11 +19,18 @@ public class AgendamentoEmailJob {
 	@Inject 
 	private AgendamentoEmailServico agendamentoEmailServico;
 	
+	@Inject
+	@JMSConnectionFactory("java:jboss/DefaultJMSConnectionFactory")
+	private JMSContext context;
+	
+	 @Resource(mappedName = "java:/jms/queue/EmailQueue")
+	 private Queue queue;
+	
 	@Schedule(hour = "*", minute = "*", second = "*/10")
 	public void enviarEmail() { 
 		List<AgendamentoEmail> listarPorNaoAgendado = agendamentoEmailServico.listarPronaoAgendado();
 		listarPorNaoAgendado.forEach(emailNaoAgendado -> {
-			agendamentoEmailServico.enviar(emailNaoAgendado);
+			context.createProducer().send(queue, emailNaoAgendado);
 			agendamentoEmailServico.alterar(emailNaoAgendado);
 		});
 	}
