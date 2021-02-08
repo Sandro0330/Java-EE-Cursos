a28f1740ffd4e82492058cd657a2acebafff8d6a
Sandro0330 <sandro030486@gmail.com>
2021-02-08 18:31:29 -0300

    Curso JSF com Primefaces configurando o servidor e iniciando a aplicação para teste

---
+++ meu-projeto/src/main/java/com/jsf/livraria/bean/LivroBean.java
@@ -0,0 +1,12 @@
+package com.jsf.livraria.bean;
+
+import jakarta.annotation.ManagedBean;
+
+@ManagedBean
+public class LivroBean {
+	
+	public void gravar() {
+		System.out.println("Gravando livro");
+	}
+
+}
